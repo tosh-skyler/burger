9823904d18413045f5805094b2693afa9132761c
Skyler Tosh <skylerjtosh@gmail.com>
2020-04-25 13:37:23 -0700

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql");
+let connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "101ruBBish10$",
-  database: "burger_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '101ruBBish10$',
+    database: 'burger_db'
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
